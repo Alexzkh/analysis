fc0dc64728b34484a2dc38fb9e91ea34164a2d20
machengjun <machengjun@zqykj.com>
2021-08-16 10:57:42 +0800

    1. 清除 test 类代码与配置

--- tldw-analysis-server/src/test/java/com/zqykj/analysis/test/EsInterfaceTest.java
+++ tldw-analysis-server/src/test/java/com/zqykj/analysis/test/EsInterfaceTest.java
@@ -3,12 +3,8 @@
  */
 package com.zqykj.analysis.test;
 
-import com.zqykj.AnalysisServiceApplication;
-import com.zqykj.analysis.controller.TestController;
 import com.zqykj.app.service.dao.ElasticTestDao;
-import com.zqykj.app.service.repository.TestEsRepository;
 import com.zqykj.domain.bank.StandardBankTransactionFlow;
-import com.zqykj.infrastructure.util.ApplicationUtils;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +24,5 @@ public class EsInterfaceTest {
     @Test
     public void testEsInterface() throws Exception {
         System.out.println(testDao.search(new MatchAllQueryBuilder(), StandardBankTransactionFlow.class).size());
-        System.out.println(ApplicationUtils.getBean(TestController.class));
     }
 }
