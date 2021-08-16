ced6575223c425b006ea895a2bda3139b131f37b
machengjun <machengjun@zqykj.com>
2021-08-16 11:07:27 +0800

    1. ElasticTestDao packages 位置调整

--- tldw-analysis-app/src/main/java/com/zqykj/app/service/dao/ElasticTestDao.java
+++ tldw-analysis-app/src/main/java/com/zqykj/app/service/dao/ElasticTestDao.java
@@ -5,7 +5,6 @@ package com.zqykj.app.service.dao;
 
 import com.zqykj.domain.bank.StandardBankTransactionFlow;
 import com.zqykj.tldw.aggregate.searching.ElasticsearchTemplateOperations;
-import org.springframework.stereotype.Repository;
 
 public interface ElasticTestDao extends ElasticsearchTemplateOperations<StandardBankTransactionFlow, String> {
 
