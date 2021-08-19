1b434008e694799e575920512370454df2ef1ea6
张克厚 <zhangkehou@zqykj.com>
2021-08-19 20:34:28 +0800

    Revert "Merge remote-tracking branch 'origin/dev_jz_2.6.4_version' into dev_jz_2.6.4_version"
    This reverts commit 2af56eb825034a24ea99d4259801717f442a1909

--- tldw-analysis-server/src/test/java/com/zqykj/AggregateDataTest.java
+++ tldw-analysis-server/src/test/java/com/zqykj/AggregateDataTest.java
@@ -9,16 +9,11 @@ import com.zqykj.domain.aggregate.TeacherInfo;
 import com.zqykj.domain.page.Page;
 import com.zqykj.domain.page.PageRequest;
 import com.zqykj.domain.page.Sort;
-import com.zqykj.domain.routing.Route;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -38,24 +33,5 @@ public class AggregateDataTest {
         System.out.println(infoPage.getTotalElements());
         System.out.println(infoPage.getTotalPages());
         System.out.println(JSON.toJSONString(infoPage.getSort()));
-    }
-
-    @Test
-    public void testSalaryAggregations() {
-        Page<TeacherInfo> teacherInfos = teacherInfoDao.testAggregate(PageRequest.of(0, 2,
-                Sort.by(new Sort.Order(Sort.Direction.DESC, "age"))), new Route("1231231"));
-        System.out.println(JSON.toJSONString(teacherInfos.getAggregations()));
-    }
-
-    @Test
-    public void testOptionalQuery() {
-        Optional<List<TeacherInfo>> teacherInfos = teacherInfoDao.matchAllofOptional();
-        System.out.println(JSON.toJSONString(teacherInfos.orElse(new ArrayList<>())));
-    }
-
-    @Test
-    public void testListQuery() {
-        List<TeacherInfo> teacherInfos = teacherInfoDao.matchAllofList();
-        System.out.println(JSON.toJSONString(teacherInfos));
     }
 }
