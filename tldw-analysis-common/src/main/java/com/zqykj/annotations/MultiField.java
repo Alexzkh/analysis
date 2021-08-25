74de4c8c4128ec71a6639df137a1859a7e4a7d7d
machengjun <machengjun@zqykj.com>
2021-08-25 15:06:59 +0800

    1. 增加Elasticsearch @MultiField 注解 支持 property Multi-fields

--- tldw-analysis-common/src/main/java/com/zqykj/annotations/MultiField.java
+++ tldw-analysis-common/src/main/java/com/zqykj/annotations/MultiField.java
@@ -3,9 +3,14 @@
  */
 package com.zqykj.annotations;
 
+import java.lang.annotation.*;
+
 /**
  * <h1> 用来处理Multi-field  </h1>
  */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface MultiField {
 
     /**
