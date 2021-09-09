b8a1067ab291894d197d244d83bc9295b8cf648a
kehou <zhangkehou@zqykj.com>
2021-09-09 11:13:24 +0800

    remove @ConditionOnMissingBean

--- tldw-aggregate-searching/src/main/java/com/zqykj/tldw/aggregate/config/AggregateDataConfiguration.java
+++ tldw-aggregate-searching/src/main/java/com/zqykj/tldw/aggregate/config/AggregateDataConfiguration.java
@@ -16,7 +16,6 @@ import com.zqykj.tldw.aggregate.searching.mongoclientrhl.MongoRestTemplate;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -66,7 +65,6 @@ public class AggregateDataConfiguration {
 
     @Bean
     @ConditionalOnBean(SimpleElasticsearchMappingContext.class)
-    @ConditionalOnMissingBean(ElasticsearchRestTemplate.class)
     public ElasticsearchRestTemplate elasticsearchRestTemplate(RestHighLevelClient restHighLevelClient,
                                                                SimpleElasticsearchMappingContext simpleElasticsearchMappingContext) {
         
